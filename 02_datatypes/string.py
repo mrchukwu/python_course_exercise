1b3aa5199a80f5d88be2a0040903381cbd4e2b30
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2026-01-22 20:15:51 +0100

    update datatypes with String exercise

---
+++ 02_datatypes/string.py
@@ -0,0 +1,13 @@
+chai_type = "Ginger chai"
+customer_name = "Ankita"
+
+print(f"Order for {customer_name} : {chai_type} is ready!")
+
+chai_description = "Aromatic and Bold"
+print(f"First word: {chai_description[0:8]}")
+print(f"last word: {chai_description[12:]}")
+print(f"Reversed string: {chai_description[:: -1]}")
+
+label_text = "Chai Special"
+encoded_label = label_text.encode("utf-8")
+print(f"Encoded label: {encoded_label}")
